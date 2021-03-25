86999a6e2575a194e4ae5fbb93bc819566269f54
Bikram Gyawali <bikramgyawali57@gmail.com>
2021-03-25 20:42:14 +0545

    routes created

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
 const expresslayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
-
+const authorRouter = require("./routes/authors");
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layouts");
 app.use(expresslayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+// require("dotenv");
 
-require("dotenv").config({ path: ".env" });
+const dotenv = require("dotenv");
+dotenv.config();
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
@@ -25,5 +29,5 @@ db.once("open", () => {
   console.log("connected to database");
 });
 app.use("/", indexRouter);
-
+app.use("/authors", authorRouter);
 app.listen(process.env.PORT || 3000);
