1a153fcd677bedde2694d6ff2437feed7c6be6ff
Bikram Gyawali <bikramgyawali57@gmail.com>
2021-03-25 21:54:44 +0545

    searching with mongo done

--- routes/authors.js
+++ routes/authors.js
@@ -3,8 +3,20 @@ const router = express.Router();
 const Author = require("../models/author");
 
 //All aurthors route
-router.get("/", (req, res) => {
-  res.render("authors/index");
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name !== "") {
+    searchOptions.name = new RegExp(req.query.name, "i");
+  }
+  try {
+    const authors = await Author.find(searchOptions);
+    res.render("authors/index", {
+      authors: authors,
+      searchOptions: req.query,
+    });
+  } catch {
+    res.redirect("/");
+  }
 });
 
 //New Aurthor Route
@@ -13,20 +25,31 @@ router.get("/new", (req, res) => {
 });
 
 //Create Aurthor Route
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const author = new Author({
     name: req.body.name,
   });
-  author.save((err, newAuthor) => {
-    if (err) {
-      res.render("authors/new", {
-        author: author,
-        errorMessage: "Error creating author",
-      });
-    } else {
-      res.redirect("authors");
-    }
-  });
+
+  try {
+    const newAuthor = await author.save();
+    res.redirect("authors");
+  } catch {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error creating author",
+    });
+  }
+
+  // author.save((err, newAuthor) => {
+  //   if (err) {
+  //     res.render("authors/new", {
+  //       author: author,
+  //       errorMessage: "Error creating author",
+  //     });
+  //   } else {
+  //     res.redirect("authors");
+  //   }
+  // });
 });
 
 module.exports = router;
